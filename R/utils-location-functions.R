1a6f534efce77e0e87b0035de079c02f9f4e1103
Evan Odell <evanodell91@gmail.com>
2020-08-03 09:10:02 +0000

    version 0.2.2

--- R/utils-location-functions.R
+++ R/utils-location-functions.R
@@ -20,39 +20,42 @@ ukc_lat_lng <- function(lat, lng) {
 
 
 utils_poly_processing <- function(poly_df) {
-  # check for SP
-  if (inherits(poly_df, "Spatial")) {
-    if (requireNamespace("sf", quietly = TRUE)) {
-      poly_df <- sf::st_as_sf(poly_df)
-    } else {
-      warning("Package \"sf\" is needed to process Spatial data.",
-        call. = TRUE, immediate. = TRUE
-      )
-    }
-  }
+  if (requireNamespace("sf", quietly = TRUE)) {
+    names(poly_df) <- tolower(names(poly_df)) # for matching functions
 
-  if (inherits(poly_df, "sf")) {
-    if (requireNamespace("sf", quietly = TRUE)) {
-      poly_df <- as.data.frame(sf::st_coordinates(poly_df, crs = 4326))
-      names(poly_df) <- c("lat", "lng")
-    } else {
-      warning("Package \"sf\" is needed to process simple features.",
-        call. = TRUE, immediate. = TRUE
-      )
+    # check for SP
+    if (inherits(poly_df, "Spatial")) {
+      poly_df <- sf::st_as_sf(poly_df, crs = 4326)
     }
-  }
 
-  if (all(c("lat", "lng") %in% names(poly_df))) {
-    poly_df2 <- poly_df[c("lat", "lng")]
+    if (inherits(poly_df, "sf")) {
+      sf::st_crs(poly_df) <- 4326
+
+      poly_df <- as.data.frame(sf::st_coordinates(poly_df))
+
+      names(poly_df)[names(poly_df) == "X"] <- "lng"
+      names(poly_df)[names(poly_df) == "Y"] <- "lat"
+    }
+
+    if (all(c("lat", "lng") %in% names(poly_df))) {
+      poly_df2 <- poly_df[c("lat", "lng")]
+    } else {
+      poly_df2 <- poly_df[c(
+        grep("^lat", names(poly_df)),
+        grep("^lon|^lng", names(poly_df))
+      )]
+
+      names(poly_df2) <- c("lat", "lng")
+    }
+
+    poly_string <- ukc_poly_paste(
+      poly_df2,
+      "lng",
+      "lat"
+    )
   } else {
-    poly_df2 <- poly_df[c(1:2)]
-    names(poly_df2) <- c("lat", "lng")
-    # might be a cleaner way to do this
+    warning("Package \"sf\" is needed to process Spatial data.",
+      call. = TRUE, immediate. = TRUE
+    )
   }
-
-  poly_string <- ukc_poly_paste(
-    poly_df2,
-    "lng",
-    "lat"
-  )
 }
--- tests/testthat/test-specific-crime-no-location-crime.R
+++ tests/testthat/test-specific-crime-no-location-crime.R
@@ -1,17 +1,18 @@
 context("test-specific-crime")
 
 test_that("specific-crime and no-location crime", {
-  # skip_on_cran()
-  no_location <- ukc_crime_no_location(
+  skip_on_cran()
+
+  no_location1 <- ukc_crime_no_location(
     force = "city-of-london",
     date = "2019-01"
   )
 
-  expect_true(all(is.na(no_location$location)))
+  expect_true(all(is.na(no_location1$location)))
 
   expect_true(
     "a832abdef7dc2a9a794ca3ce9730e541619c316e6cc87132334a50ec7762b8ae" %in%
-      no_location$persistent_id
+      no_location1$persistent_id
   )
 
   outcome <- ukc_specific_outcome(
@@ -19,14 +20,6 @@ test_that("specific-crime and no-location crime", {
   )
 
   expect_length(outcome, 2)
-
-  outcome2 <- ukc_specific_outcome(
-    "590d68b69228a9ff95b675bb4af591b38de561aa03129dc09a03ef34f537588c"
-  )
-
-  expect_length(outcome2, 2)
-  expect_true(is.list(outcome2))
-
 
   expect_message(
     ukc_specific_outcome("asdffsda"),
